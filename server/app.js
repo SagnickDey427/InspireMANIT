ca7abc4486de872fc30d6dddefc94c3ec1dc2cd9
Sagnick Dey <sagnickdey427@gmail.com>
2026-02-19 21:11:58 +0530

    Update CORS origin to production URL

--- server/app.js
+++ server/app.js
@@ -19,7 +19,7 @@ app.use(express.static(path.join(__dirname, '../client/dist')));
 // });
 
 const corsOptions = {
-    origin : ["http://localhost:5173"]
+    origin : ["faithful-presence-production.up.railway.app"]
 }
 
 app.use(cors(corsOptions));
